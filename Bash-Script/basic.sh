86368effddaf2674b1ee2bf4dd858786c4db8a30
Satish Radadiya <satishradadiya@gmail.com>
2019-07-11 16:59:20 +0530

    Basic Tips and Rules for beginner

---
+++ Bash-Script/basic.sh
@@ -0,0 +1,18 @@
+# ! /bin/bash
+
+# This script file contain some basic rules for Learning and Understanding Purpose.
+# Fist File for Beginner,
+
+
+#To Print your message
+
+echo "Hello world"
+echo "Welcome to the Linux Canto"
+echo "We know the Linux POEM"
+
+
+#to use linux command
+
+pwd
+ls=/bin/ls
+echo "hello $ls ,  how are you?"
